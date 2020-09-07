c969605509e13f717f6f17b0918fd880f4fb7277
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-06 22:20:37 -0300

    login page design

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
+import SigIn from '../pages/SignIn';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route path="/" exact component={SigIn} />
     <Route path="/import" component={Import} />
   </Switch>
 );
